669bbfe988c9eb5cc54df2c4c6a3aa87c80f6601
nezaicev <nezaicev@yandex.ru>
2019-12-16 01:31:04 +0300

    Добавлен gunicorn

--- Account/settings.py
+++ Account/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=%m_cox)igrua#6%ubdolnj7^ccb*ytiz##o%&=+&3s8tww^!q'
+SECRET_KEY = secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
