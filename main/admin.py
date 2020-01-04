7f76707f87a9f8b778553806436c4dccbca10939
nezaicev <nezaicev@yandex.ru>
2020-01-05 02:03:37 +0300

    Добавлена функция 'Выгрузка списка'

--- main/admin.py
+++ main/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.conf import settings
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.core.mail import EmailMultiAlternatives, get_connection
-import secret
+from django.http import HttpResponseRedirect, HttpResponse
+
+import csv
 from main.tasks import send_mails_admin_tacks
 from main.models import Artakiada,NRusheva,Mymoskvichi,Teacher
-from main.forms import TextEditor,MymoskviciContestForm,EmailForm
+from main.forms import TextEditor
 
 # Register your models here.
 
@@ -23,32 +22,55 @@ class BaseAdmin(admin.ModelAdmin):
 
     send_emails.short_description = 'Отправить письмо'
 
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row=[]
+            for field in obj._meta.fields:
+                if field.choices:
+                    val = obj._get_FIELD_display(field)
+                    row.append(val)
+                else:
+                    val = getattr(obj, field.name)
+                    row.append(val)
+            writer.writerow(row)
+        return response
+
+    export_as_csv.short_description = 'Выгрузить список'
+
 
 class ArtakiadaAdmin(BaseAdmin):
     search_fields = ('reg_number','email','fio','fio_teacher')
     list_display = ('reg_number','status', 'fio',  'school', 'region','district','fio_teacher','teacher',)
     list_editable = ('status',)
-    actions=['send_emails']
+    actions=['send_emails','export_as_csv']
 
 
 class NRushevaAdmin(BaseAdmin):
     search_fields = ('reg_number', 'email', 'fio','fio_teacher')
     list_display = ('reg_number', 'status', 'fio', 'school', 'region', 'district', 'fio_teacher', 'teacher',)
     list_editable = ('status',)
-    actions = ['send_emails']
+    actions = ['send_emails','export_as_csv']
 
 
 class MymoskvichiAdmin(BaseAdmin):
     search_fields = ('reg_number', 'email', 'fio','fio_teacher')
     list_display = ('reg_number', 'status', 'fio_teacher', 'school', 'region', 'district', 'fio', 'teacher',)
     list_editable = ('status',)
-    actions = ['send_emails']
+    actions = ['send_emails','export_as_csv']
 
 
 class TeacherAdmin(BaseAdmin):
     search_fields = ('email','fio',)
     list_display = ('fio', 'school', 'email','region', 'district','status',)
-    actions=['send_emails']
+    actions=['send_emails','export_as_csv']
 
 
 admin.site.register(Artakiada, ArtakiadaAdmin)
