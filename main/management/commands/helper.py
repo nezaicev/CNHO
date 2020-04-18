07adeaee6691b0c3bd6a6a5a1a25b399510dade2
nezaicev <nezaicev@yandex.ru>
2020-04-18 20:40:20 +0300

    Хелпер для сопоставления изображений и заявок Артакиада

---
+++ main/management/commands/helper.py
@@ -0,0 +1,32 @@
+import os
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from main.models import Artakiada
+
+
+class Command(BaseCommand):
+    help = 'Сопоставление изображений по фамилиям'
+
+    def add_arguments(self, parser):
+        parser.add_argument('path', nargs='+', type=str)
+
+    def handle(self, *args, **kwargs):
+        directory=kwargs["path"][0]
+        files=os.listdir(directory)
+        for f in files:
+            fio=f.split()[0]
+
+            user_list=Artakiada.objects.filter(fio__contains=fio).values()
+            if user_list:
+                for i in user_list:
+                    print(f)
+                    print(i['fio'],i['fio_teacher'],i['school'],i['reg_number'])
+                    q=input("Введите y или n :")
+                    if q=='y':
+                        user=Artakiada.objects.get(reg_number=user_list[0]['reg_number'])
+                        user.image=os.path.join(directory,f)
+                        user.status='5'
+                        user.save()
+                        break
+                    elif q=='n':
+                        continue
